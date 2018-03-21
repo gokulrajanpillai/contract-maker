c23b0487d2dc5af07f599801fe0d3637b3165755
gokulrajanpillai <gokulrajanpillai@gmail.com>
2018-03-21 14:09:27 +0100

    Refactored code with separate constants file and folder locations

--- ContractApplikation/Src/Helper/Constants.cs
+++ ContractApplikation/Src/Helper/Constants.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContractApplikation.Src.Helper
 {
-    class Class1
+    public class Constants
     {
+        public static readonly string CURRENCY_SYMBOL = "€";
+
+        public static readonly string FILE_PATH_FILLER = "\\";
+
+        public struct FileLocation
+        {
+
+            private static readonly string PROJECT              = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            private static readonly string PROJECT_DATA         = PROJECT + FILE_PATH_FILLER + "Data";
+
+            private static readonly string OUTPUT               = PROJECT + FILE_PATH_FILLER + "Output";
+
+            public static readonly string DATASOURCE            = PROJECT_DATA + FILE_PATH_FILLER + "Vertrag-DB.accdb";
+
+            public static readonly string PROTOTYPE_CONTRACT    = PROJECT_DATA + FILE_PATH_FILLER + "PrototypeVertrag.docx";
+
+            public static readonly string PROTOTYPE_COSTTABLE   = PROJECT_DATA + FILE_PATH_FILLER + "TabelleKosten.xlsx";
+
+            public static readonly string OUTPUT_FILE           = OUTPUT + FILE_PATH_FILLER + "Finished_Contract.docx";
+
+            public static string OutputFilePath(string filename)
+            {
+                return OUTPUT + FILE_PATH_FILLER + filename;
+            }
+        }
     }
 }
